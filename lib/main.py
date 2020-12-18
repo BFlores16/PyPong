6ae0ba0e30fe7bc5210c0e24f8296edd43851ff9
Brando <brandoflores16@gmail.com>
2020-12-17 18:01:51 -0700

    Add Ball and initial movement

--- lib/main.py
+++ lib/main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from Paddle import Paddle
+from Ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=1500, height=1000)
@@ -9,7 +11,7 @@ screen.title("PyPong")
 
 right_paddle = Paddle((700, 0))
 left_paddle = Paddle((-700, 0))
-
+ball = Ball()
 
 screen.listen()
 screen.onkey(right_paddle.up, "Up")
@@ -20,8 +22,9 @@ screen.onkey(left_paddle.down,"s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
-
+    ball.move()
 
 
 
