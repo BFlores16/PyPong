37ea3361c3aecc3cc1fc0fd38928daa1def4e1e6
Brando <brandoflores16@gmail.com>
2020-12-17 19:24:11 -0700

    Add scoreboard and score tracking

--- lib/main.py
+++ lib/main.py
@@ -1,6 +1,7 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from Paddle import Paddle
 from Ball import Ball
+from Scoreboard import Scoreboard
 import time
 
 SCREEN_HEIGHT = 1000
@@ -15,34 +16,36 @@ screen.title("PyPong")
 right_paddle = Paddle((SCREEN_WIDTH * 0.5 - 50, 0))
 left_paddle = Paddle((SCREEN_WIDTH * -0.5 + 50, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(right_paddle.up, "Up")
 screen.onkey(right_paddle.down, "Down")
 screen.onkey(left_paddle.up, "w")
-screen.onkey(left_paddle.down,"s")
-
+screen.onkey(left_paddle.down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.05)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
-    #Detect collision with wall
+    # Detect collision with wall
     if ball.ycor() > (SCREEN_HEIGHT - 520) or ball.ycor() < (SCREEN_HEIGHT * -1 + 520):
         ball.bounce_y()
 
-    #Detect collisions with paddles
-    if ball.distance(right_paddle) < 50 and ball.xcor() > (SCREEN_WIDTH - 820) or ball.distance(left_paddle) < 50 and ball.xcor() < -670:
+    # Detect collisions with paddles
+    if ball.distance(right_paddle) < 50 and ball.xcor() > (SCREEN_WIDTH - 820) or ball.distance(
+            left_paddle) < 50 and ball.xcor() < -670:
         ball.bounce_x()
 
-    #Detect if ball misses the screen
+    # Detect if ball misses the paddles
     if ball.xcor() > SCREEN_WIDTH - 750:
+        scoreboard.left_scores()
         ball.reset()
+
     if ball.xcor() < SCREEN_WIDTH * -0.5:
+        scoreboard.right_scores()
         ball.reset()
-
-
 
 screen.exitonclick()
